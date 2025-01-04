f25267146686212c0234665ad73afd7d283f2191
raysas <rayane.s.adam@gmail.com>
2025-01-04 23:50:37 +0100

    troubleshooting: module, loss & optim clear

--- src/module.py
+++ src/module.py
@@ -1,49 +1,41 @@
+from abc import ABC, abstractmethod
 from parameter import Parameter
 
-class Module:
-    def __init__(self):
-        self._modules = {}
-        self._parameters = {}
-
-    def __setattr__(self, name, value):
-        if isinstance(value, Module):
-            self._modules[name] = value
-        elif isinstance(value, Parameter):
-            self._parameters[name] = value
-        else:
-            super().__setattr__(name, value)
-
-    def __getattr__(self, name):
-        
-        if "_parameters" in self.__dict__:
-            _parameters = self.__dict__["_parameters"]
-            if name in _parameters:
-                return _parameters[name]
-            
-        if "_modules" in self.__dict__:
-            modules = self.__dict__["_modules"]
-            if name in modules:
-                return modules[name]
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{name}'"
-        )
-
-    def parameters(self):
-        for _, param in self._parameters.items():
-            yield param
-        for _, module in self._modules.items():
-            yield from module.parameters()
-
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
-
-    def __repr__(self):
-        module_str = ', '.join(f"{name}" for name, module in self._modules.items())
-        return f"{self.__class__.__name__}({module_str})"
+class Module(ABC):
     
-    # def to(self, device):
-    #     # Move each parameter to the device (e.g., GPU or CPU)
-    #     for param in self.parameters:
-    #         param.data = param.data.to(device)
+    def __init__(self):
+        self._subclasses={} #dictionary to save all the initialized instances of the classes that inherit Module like the linear layers and the activation functions 
+        self._parameters={} #dictionary to register the parameters (weights and biases) of the model
         
-    #     return self 
+        
+    def __setattr__(self, name, value): #to automatically register the subclasses (layers and activation functions) and parameters when they are assigned
+        if isinstance(value, Module):
+            self._subclasses[name]=value
+        if isinstance(value, Parameter): 
+            self._parameters[name]=value
+        super().__setattr__(name, value)
+        
+        
+    def parameters(self): #to be called through model.parameters()
+        '''
+        Returns a list of all the parameters of all the layers of the model
+        '''
+        parameters_list=[]
+        for parameter in self._parameters.values():
+            parameters_list.append(parameter)
+        for layer in self._subclasses.values(): #go through each layer and add its parameters
+            parameters_list.extend(layer.parameters())
+        return parameters_list
+
+
+    @abstractmethod
+    def forward(self, x): #to be implemented by the subclasses (Model, Linear, Activation)
+        pass
+    
+    
+    def __call__(self, x): #modified to return the forward method implemented by the subclass
+        return self.forward(x)
+    
+    
+    def __repr__(self): #to visualize the neural network architechture 
+        return f"{self.__class__.__name__}({self._subclasses})"   
