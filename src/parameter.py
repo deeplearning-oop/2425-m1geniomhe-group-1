0524ef572acb6f5e30bfcfbb1266c235cb329281
yazid-hoblos <yazid.hoblos@lau.edu>
2025-01-04 08:24:59 +0100

    functional prototype

---
+++ src/parameter.py
@@ -0,0 +1,8 @@
+from tensor import Tensor
+
+class Parameter(Tensor):
+    def __init__(self, value):
+        super().__init__(value, requires_grad=True, is_leaf=True)
+        
+    def __str__(self):
+        return str(self.data)
