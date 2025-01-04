502561f7e62a577720ad7a05ea68143830a630b3
raysas <rayane.s.adam@gmail.com>
2025-01-04 23:14:19 +0100

    -- added opt and loss from prototype3 to test

---
+++ src/optimizer.py
@@ -0,0 +1,42 @@
+import numpy as np
+from tensor import Tensor
+
+class Optimizer:
+    def __init__(self, parameters, lr=0.01):
+        self.parameters = [p for p in parameters]
+        self.lr = lr
+
+    # def step(self):
+    #     for p in self.parameters:
+    #         if p.grad is not None:
+    #             p.data -= p.grad * self.lr
+
+    def zero_grad(self):
+        for p in self.parameters:
+            if p.grad is not None:
+                p.grad = None
+
+class SGD(Optimizer):
+    def __init__(self, parameters, lr=0.01, momentum=0.0):
+        super().__init__(parameters, lr)
+        self.momentum = momentum
+        self.velocities = {id(p): 0 for p in self.parameters}
+
+    def step(self):
+        i=1
+        for p in self.parameters:
+            # if p.grad is None:
+                # continue
+            # if i % 2 == 0:
+                # p.grad = np.sum(p.grad,axis=1).reshape(-1,1)
+                # p.grad = np.sum(p.grad)
+            
+            # p.grad/=42000
+            # Ensure that velocity matches the shape of the parameter
+            # p.grad = Tensor(p.grad)
+            # velocity = - (p.grad * self.lr) + (self.velocities[id(p)] * self.momentum)
+            # self.velocities[id(p)] = velocity
+            # Update the parameter with the velocity
+            # p.data = velocity.data + p.data
+            p.data -= p.grad * self.lr
+            i+=1
---
+++ src/test_with_dataloaders.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from module import Module
+from linear import Linear
+from optimizer import SGD
+from loss import CrossEntropyLoss, MSE
+
+from tensor import Tensor
+import numpy as np
+
+from new_dataset import MNIST
+from new_dataloader import DataLoader
+
+# -- using our implemented dataset module
+train_data = MNIST(root='data/', train=True, download=True)
+test_data = MNIST(root='data/', train=False, download=True)
+
+# -- using our implemented dataloader module
+train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=32, shuffle=True)
+
+# -- model definition
+class Model(Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = Linear(20, 28*28)
+        self.linear2 = Linear(10, 20)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = x.relu()
+        x = self.linear2(x)
+        return x.softmax()
+    
+model = Model()
+optimizer = SGD(model.parameters(), lr=0.1, momentum=0.9)
+loss_fn = CrossEntropyLoss()
+
+# -- training
+
+for epoch in range(5):
+    for batch_no,(x, y) in enumerate(train_loader):
+        # (32, 1, 28, 28)
+        x=x.flatten_batch() # (784, 32)
+        x=x/255
+        optimizer.zero_grad()
+        y_hat = model(x)
+        loss = loss_fn(y, y_hat)
+        loss.backward()
+        optimizer.step()
+        
+
+    print(f'iteration: {epoch}')    
+    print(f'Loss: {loss.data}') 
+    predictions = np.argmax(y_hat.data, axis=0)
+    accuracy = np.sum(predictions == y.data) / y.data.size
+    print(predictions, y.data)
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+    print('------------------')
