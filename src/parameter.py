12876f57fb0bd232d9c11c6a2025232e2462bb0a
JoelleAs <joelle.assy@lau.edu.com>
2025-01-05 22:39:40 +0100

    -

--- src/parameter.py
+++ src/parameter.py
@@ -3,7 +3,7 @@ from tensor import Tensor
 class Parameter(Tensor):
     
     def __init__(self, data):
-        super().__init__(data, requires_grad=True, is_leaf=True)
+        super().__init__(data, requires_grad=True)
         
     def __repr__(self):
         return f"Parameter(data={self.data})"
