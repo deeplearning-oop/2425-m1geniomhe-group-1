e36d33c69ce07304efa58cf581e89bb7942d202b
yazid-hoblos <yazid.hoblos@lau.edu>
2025-01-05 14:21:58 +0100

    updating backward to decorator in Tensor, Activation, Loss

--- src/loss.py
+++ src/loss.py
@@ -12,73 +12,81 @@ class Loss(Module):
 
     def __call__(self, y, y_hat):
         return self.forward(y, y_hat)
+    
+    @staticmethod
+    def backward_decorator(loss_type):
+        def decorator(func):
+            def wrapper(self, y, y_hat):
+                # Perform the forward pass to create the output
+                out = func(self, y, y_hat)
+                # Attach the grad function to the result tensor
+                out.grad_fn = lambda grad: self.grad_compute(loss_type, y, y_hat)
+                out.grad_fn_name = f"{loss_type}Backward"
+                # Define the parents (same as in the Tensor class)
+                out.parents = {y, y_hat}
+                return out
+            return wrapper
+        return decorator
+
+    def grad_compute(self, loss_type, y, y_hat):
+        """
+        Centralized gradient computation for different loss functions.
+        """
+        if loss_type == "MSE":
+            # Gradient of MSE loss with respect to y_hat
+            batch_size = y_hat.data.shape[0]
+            grad_input = 2 * (y_hat.data - y.data) / batch_size
+
+        elif loss_type == "CrossEntropyLoss":
+            
+            # One-hot encoding of y
+            one_hot_y = np.zeros((y.data.size, y_hat.data.shape[0]))
+            one_hot_y[np.arange(y.data.size), y.data] = 1
+            one_hot_y = one_hot_y.T
+            
+            grad_input = - (one_hot_y / y_hat.data) / y.data.size
+        
+        y_hat.grad = grad_input if y_hat.grad is None else y_hat.grad + grad_input
+
+        # Add more loss functions here if needed
+        return  y_hat.grad
+         
 
 class MSE(Loss):
+    @Loss.backward_decorator("MSE")
     def forward(self, y, y_hat):
         """
         y: Tensor of shape (batch_size, num_outputs) (target labels)
         y_hat: Tensor of shape (batch_size, num_outputs) (predicted values)
         """
         # Compute Mean Squared Error
-        batch_size = y_hat.data.shape[0]
         error = y_hat.data - y.data
         loss = np.mean(error ** 2)
-        
-        # Create the output tensor with requires_grad=True for gradient computation
-        out = Tensor(loss, requires_grad=True)
-        
-        # Keep track of the parents
-        out.parents = {y, y_hat}
-        
-        # Backward function to compute gradients
-        def _backward(grad):
-            
-            # Gradient of the loss w.r.t y_hat
-            grad_input = 2 * (y_hat.data - y.data) / batch_size
-            
-            if y_hat.grad is None:
-                y_hat.grad = grad_input
-            else:
-                y_hat.grad += grad_input
+        return Tensor(loss, requires_grad=True, is_leaf=False)
 
-        out.grad_fn = _backward
-        out.grad_fn_name = "MSEBackward"
-        
-        return out
+    def __repr__(self):
+        return "MSE()"
     
 
 class CrossEntropyLoss(Loss):
+    @Loss.backward_decorator("CrossEntropyLoss")
     def forward(self, y, y_hat):
-        
-        # adding epsilon to avoid log(0)
+        """
+        y: Tensor of shape (batch_size, num_outputs) (target labels)
+        y_hat: Tensor of shape (batch_size, num_outputs) (predicted values)
+        """
+        # Adding epsilon to avoid log(0)
         epsilon = 1e-15
         y_pred = np.clip(y_hat.data, epsilon, 1 - epsilon)
-        
-        # one hot encoding y
+
+        # One-hot encoding y
         one_hot_y = np.zeros((y.data.size, y_hat.data.shape[0]))
         one_hot_y[np.arange(y.data.size), y.data] = 1
         one_hot_y = one_hot_y.T
-        
+
         loss = -np.sum(one_hot_y * np.log(y_pred)) / y.data.size
+        return Tensor(loss, requires_grad=True, is_leaf=False)
+    
+    def __repr__(self):
+        return "CrossEntropyLoss()"
         
-        out = Tensor(loss, requires_grad=True)
-        
-        # Keep track of the parents
-        out.parents = {y, y_hat}
-        
-        # Backward function to compute gradients
-        def _backward(grad):
-                        
-            # grad_input = (y_hat.data - one_hot_y) / y.data.size
-            grad_input = - (one_hot_y / y_pred) / y.data.size
-            
-            if y_hat.grad is None:
-                y_hat.grad = grad_input
-            else:
-                y_hat.grad += grad_input
-
-        out.grad_fn = _backward
-        out.grad_fn_name = "CrossEntropyLossBackward"
-        
-        return out
-
